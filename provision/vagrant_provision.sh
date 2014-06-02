b9998939eb0ee79583a5c8791036dca6cdc33ebb
Justin C. Miller <justin@devjustinian.com>
2014-06-02 14:49:35 -0700

    Extra provisioning work - SSH keys, dotfiles

--- provision/vagrant_provision.sh
+++ provision/vagrant_provision.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+# Add sources from vagrant_sources.txt, if there are any
 xargs -r -a /vagrant/provision/vagrant_sources.txt -- add-apt-repository -y
 
+# Get all latest package info from sources
 echo -n "Updating apt package lists..."
 if apt-get -qq update; then
 	echo " Done."
@@ -9,5 +11,15 @@ else
 	echo " ERROR!"
 fi
 
+# Install packages from vagrant_packages.txt and clean up leftovers
 xargs -r -a /vagrant/provision/vagrant_packages.txt -- apt-get install -y -q
 apt-get autoremove -y
+
+# Install all my SSH keys
+mkdir -p /home/vagrant/.ssh
+cat /vagrant/provision/authorized_keys >> /home/vagrant/.ssh/authorized_keys
+
+# Install my dotfiles
+DOTS_GIT="https://justinian@bitbucket.org/justinian/dotfiles.git"
+sudo -u vagrant git clone $DOTS_GIT /home/vagrant/.dotfiles
+sudo -u vagrant /home/vagrant/.dotfiles/.install
